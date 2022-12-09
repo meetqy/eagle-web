57b5cb4e10c0762124966fe5eb7c192f3833554c
meetqy <xiaoqiyang@xique.com>
2022-12-09 15:30:26 +0800

    chore: title

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import { selectMetadata, selectTags } from "@/hooks";
 import { activeMenuState, metadataState, tagsState, themeState } from "@/store";
 import { Layout, ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
+import Head from "next/head";
 import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import SiderBasic from "./sider/basic";
@@ -28,37 +29,44 @@ export default function App({ children }: { children: JSX.Element }) {
   }, [tags]);
 
   return (
-    <ConfigProvider locale={zhCN}>
-      <Layout
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        hasSider
-      >
-        <Sider width={240} theme={themeMode} className="sider-menu">
-          <SiderMenu />
-        </Sider>
-        <Content
-          className="scroll-bar main"
+    <>
+      <Head>
+        <title>rao.pics</title>
+        <meta property="og:title" content="rao.pics" key="title" />
+      </Head>
+
+      <ConfigProvider locale={zhCN}>
+        <Layout
           style={{
-            position: "relative",
-            overflowY: "auto",
-            overflowX: "hidden",
+            width: "100%",
+            height: "100%",
           }}
+          hasSider
         >
-          {children}
-        </Content>
-        <Sider
-          className="sider-basic"
-          collapsed={activeMenu?.basic}
-          collapsedWidth={0}
-          width={240}
-          theme={themeMode}
-        >
-          <SiderBasic />
-        </Sider>
-      </Layout>
-    </ConfigProvider>
+          <Sider width={240} theme={themeMode} className="sider-menu">
+            <SiderMenu />
+          </Sider>
+          <Content
+            className="scroll-bar main"
+            style={{
+              position: "relative",
+              overflowY: "auto",
+              overflowX: "hidden",
+            }}
+          >
+            {children}
+          </Content>
+          <Sider
+            className="sider-basic"
+            collapsed={activeMenu?.basic}
+            collapsedWidth={0}
+            width={240}
+            theme={themeMode}
+          >
+            <SiderBasic />
+          </Sider>
+        </Layout>
+      </ConfigProvider>
+    </>
   );
 }
